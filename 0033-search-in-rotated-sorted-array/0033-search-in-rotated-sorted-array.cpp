406e8c62501c54688a7f5c15800a496530de9e02
RAHUL SAXENA <116511365+rahulSaxena28@users.noreply.github.com>
2026-07-02 19:07:17 +0530

    Time: 0 ms (100%), Space: 15.1 MB (67.38%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,10 +1,45 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int i=0;
-        for(int i=0;i<nums.size();i++){
-            if(target==nums[i]) return i;
+    int bs(vector<int>& nums, int s, int e, int target) {
+        while (s <= e) {
+            int m = s + (e - s) / 2;
+
+            if (nums[m] == target)
+                return m;
+            else if (nums[m] < target)
+                s = m + 1;
+            else
+                e = m - 1;
         }
         return -1;
+    }
+
+   int rotatedBs(vector<int>& nums) {
+    int s = 0;
+    int e = nums.size() - 1;
+
+    while (s < e) {
+        int m = s + (e - s) / 2;
+
+        if (nums[m] > nums[e])
+            s = m + 1;
+        else
+            e = m;
+    }
+
+    return s;
+}
+
+    int search(vector<int>& nums, int target) {
+        if (nums.empty())
+            return -1;
+
+        int pivot = rotatedBs(nums);
+
+        int ans = bs(nums, 0, pivot - 1, target);
+        if (ans != -1)
+            return ans;
+
+        return bs(nums, pivot, nums.size() - 1, target);
     }
 };
