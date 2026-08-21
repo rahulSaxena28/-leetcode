231e0e4278cdb393420a9112931ed3ceae7fd619
RAHUL SAXENA <116511365+rahulSaxena28@users.noreply.github.com>
2026-08-21 11:33:33 +0530

    Time: 7 ms (20.46%) | Memory: 11 MB (68.18%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char,int> mp;
+        unordered_map<char,int> mp;
         for(auto it:s){
             mp[it]++;
         }
